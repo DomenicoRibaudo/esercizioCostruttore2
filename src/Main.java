72424b0ab1acee00f454da67c5fbb997007e10c9
Domenico <domenicoribaudo06@gmail.com>
2023-11-29 19:23:04 +0100

    prima commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Auto auto1 = new Auto(2000, "AB 123 CD", "Audi", "a4");
+        System.out.println(("Auto1 " + auto1));
+    }
+}
